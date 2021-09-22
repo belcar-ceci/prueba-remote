c84303514a0961f8a2faa6234e3a0db31f20f2e5
Ceci <carbel.cecilia@gmail.com>
2021-09-23 01:25:42 +0200

    Sprint Solution-Nivel1 & Nivel3

--- NIVEL1/js/checkout.js
+++ NIVEL1/js/checkout.js
@@ -1,56 +1,65 @@
-function validate() {
-const password = document.querySelector(".password").value;
-const phone = document.querySelector('.phone').value;
-const email = document.querySelector(".email").value;
-const name = document.querySelector('.name').value;
-const formulario =document.querySelector(".form");
+// Exercise 9
 
-formulario.addEventListener('submit', e => {
-    e.preventDefault();
-    
-    if(name.lenght == 0 || /^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(name)){
-        alert("Sólo se aceptan letras");
-        return false;
-    }else {
-        return true;
-    }
-    
-    
-
-   if(email.lenght ==0 || /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
-        alert("El email es incorrecto");
-        return false;
-   }
-   return true;
-    
-   if(password.lenght == 0 || /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[#?!@$%^&*\-_]).{8,}$/.test(password)){
-       alert("debes rellenarlo todo")
-       return false;
-   }
-   return true;
-
-   if(phone.lenght == 0 || /^\d{7,14}$/.test(phone)){
-    alert("debes rellenarlo todo")
-    return false;
-}
-return true;
-})
-}
-
-
-
-
-// Get the input fields
-/*var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+var password = document.querySelector(".password");//password
+var phone = document.querySelector('.phone');//teléfono
+var firtsName = document.querySelector('.name'); //nombre
+var inputForm = document.querySelectorAll('.input'); // Acceder a input del html del fomrulario
+var lastName = document.querySelector('#lastName'); //Apellidos
+var email = document.querySelector('#email'); //Email
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById('errorName');  
 var errorPhone = document.getElementById('errorPhone');  
 
-// Exercise 9
+
+/*https://content.breatheco.de/es/lesson/regex-tutorial-regular-expression-examples*/
+
 function validate() {
-    // Validate fields entered by the user: name, phone, password, and email
-}*/
+
+var formulario = document.querySelector("form");
+
+formulario.addEventListener('submit', (e) => {
+    inputForm.forEach(element => {
+
+
+    if (element.value == '') { // sin completar, no envia
+        e.preventDefault(); 
+        element.classList.add("invalid"); 
+    }
+    /* Al menos 3 carácters */
+    else if (element.value.length < 3) {
+        e.preventDefault(); 
+        element.classList.add("invalid"); // añadir una clase
+    } 
+   
+    /* Nombre contener solo letras*/
+    if (/^[a-z ,.'-]+$/i.test(firtsName.value)) {
+        e.preventDefault(); // 
+        firtsName.classList.add("invalido"); // añadir una clase
+    }
+    /* Cognom debe contener solo letras*/
+    if (/^[a-z ,.'-]+$/i.test(lastName.value)) {
+        e.preventDefault(); 
+        lastName.classList.add("invalido"); 
+   }
+   /* Teléfono solo números*/
+    if (0 || /^\d{7,14}$/.test(phone.value)){
+        e.preventDefault(); 
+        phone.classList.add("invalido"); 
+    }
+    /* Password incluye letras y números*/
+    if (0 || /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[#?!@$%^&*\-_]).{8,}$/.test(password.value)){
+        e.preventDefault(); 
+        password.classList.add("invalido"); 
+    }
+    /* Email formato de email @ /.com */
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)){
+        e.preventDefault(); // Event anular envío de fomulario
+        email.classList.add("invalido"); // añadir una clase
+    }
+}); //previene  envío de fomulario
+    
+});
+
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+# Sprint 3 IT Academy | Shop📌
+
+## Introduction 🍀
+
+A company in the e-commerce sector has asked us for a web application that allows them to offer the purchase of their products through the internet.
+
+You will be in charge of setting up an initial demo version of the application for the client: management of the shopping cart and the application of the promotions on the final price. You have 2 weeks to finish, which is how long this sprint lasts.
+
+## Sprint 3 Shop - Javascript 🔩:
+
+### Objetivos 
+
+  1. Poner en practica conceptos de Javascript.
+  2. Aprender a manipular set de datos.
+  3. Dividir nuestro codigo en funciones, y saber reutilizarlas.
+
+## Comenzando 🚀
+Estas instrucciones te permitirán obtener una copia del proyecto en funcionamiento en tu ordenador local para propósitos de desarrollo y pruebas:
+
+Abre la terminal y ejecuta: git clone https://github.com/belcar-ceci/sprint_3_remote.git para descargar el proyecto.
+## Dependencias 🔧
+Para los tres niveles se utiliza javascript, y Bootstrap el cual ya esta instalado
+
+## Construido con 🛠️
+Tecnologías:
+
+* HTML
+* CSS
+* Bootstrap 5
+* Javascript
+
+## Autores ✒️
+
+* Cecilia Carbajal Belmont - Sprint2_IT Academy_React_FrontEnd - belcar-ceci
+## Gratitud 🎁
+* Comenta a otros sobre este proyecto 📢 .
+* Apoya mis proyectos 🤓 .
+* ⌨️ con ❤️ por belcar-ceci 😊
+
+
+
